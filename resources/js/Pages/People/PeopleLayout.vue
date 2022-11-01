84f8f50a0e87e9ee36c126cdbc2fcadac6a9e355
Mr.H <romarhegu@gmail.com>
2022-10-31 21:12:42 -0300

    First Commit

---
+++ resources/js/Pages/People/PeopleLayout.vue
@@ -0,0 +1,29 @@
+<script>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+export default {
+    components: {
+        AuthenticatedLayout,
+    },
+};
+
+</script>
+<template>
+
+ <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        Personas
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+
+
+<style>
+
+</style>
